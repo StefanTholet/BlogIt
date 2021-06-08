dbb256a44f975ba9493e5c24a72f9635a8848e69
StefanTholet <stefan.tholet@gmail.com>
2021-06-08 15:08:33 +0300

    Restyling of comments (in progress)

--- client/src/Components/Blog/Comments/OldComment.js
+++ client/src/Components/Blog/Comments/OldComment.js
@@ -1,40 +1,55 @@
 import { Avatar, Grid, Paper } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() => ({
-    'new-comment': {
+import Divider from '@material-ui/core/Divider';
+const useStyles = makeStyles((theme) => ({
+    'old-comment': {
         color: 'black',
-        width: '30%',
+        width: '60%',
         minWidth: '28rem',
-        minHeight: '5rem',
+        minheight: '228px',
         margin: '0 auto',
         marginBottom: '3rem',
         display: 'flex',
         flexDirection: 'column',
+    },
+    'comment-header': {
+        width: '100%',
+        // display: 'flex',
+    },
+    'meta-data': {
+        width: '100%',
+        padding: '0 11px 4px',
+        display: 'flex',
+        justifyContent: 'space-between',
         alignItems: 'center'
     },
-    'comment-form': {
-        width: '100%',
+    'author-container': {
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        paddingTop: '40px'
+
     },
     'comment-date': {
         textAlign: "left",
         color: "gray",
-        marginTop: '15%'
     },
     author: {
-        // marginTop: '5px',
-        textAlign: "left",
+        textAlign: "center",
         fontSize: '0.8rem',
         fontWeight: '500',
         lineHeight: '0.8rem'
     },
     content: {
         textAlign: "left",
-        
-    }
+        paddingBottom: '40px'
+    },
+    divider: {
+        width: '100%',
+        marginBlock: '1rem'
+    },
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+      },
 }))
 
 const OldComment = ({ comment }) => {
@@ -42,22 +57,30 @@ const OldComment = ({ comment }) => {
     const classes = useStyles();
 
     return (
-        <Paper style={{ marginBottom: '2rem' }} className={classes['new-comment']}>
-            <Grid container wrap="nowrap" style={{ height: '100%' }} spacing={2}>
-                <Grid item>
-                    <Avatar alt="Remy Sharp" src={comment.avatar} />
-                </Grid>
-                <Grid justifyContent="left" style={{ height: '100%' }} item xs zeroMinWidth>
-                    <h4 className={classes.author} >{comment.author}</h4>
-                    <p className={classes.content} style={{}}>
-                        {comment.content}
-                    </p>
-                    <p className={classes['comment-date']}>
-                        Posted on: {comment.postedOnDate}
-                    </p>
-                </Grid>
-            </Grid>
-        </Paper>
+        <Grid container className={classes['old-comment']} wrap="nowrap" style={{ height: '100%' }} spacing={2}>
+            <Divider className={classes.divider} />
+            <div className={classes["comment-header"]}>
+                <div class={classes['author-container']}>
+                    <Avatar className={classes.large} alt="Remy Sharp" src={comment.avatar} />
+                    <div className={classes['meta-data']}>
+                        <h3 className={classes.author}>{comment.author}</h3>
+                        <p className={classes['comment-date']}>
+                            Posted on: {comment.postedOnDate}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div style={{ height: '100%' }}>
+
+
+                <p className={classes.content} style={{}}>
+                    {comment.content}
+                </p>
+
+            </div>
+            <Divider className={classes.divider} />
+        </Grid>
     );
 }
 
